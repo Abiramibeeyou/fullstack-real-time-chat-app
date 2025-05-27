14daac886fc40706268b1302403e9c8a31c69145
Abi <abiramib14122004@gmail.com>
2025-05-27 13:10:15 +0530

    Fix NODE_ENV reference bug

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,7 +31,7 @@ app.use("/api/auth",authRoutes);
 
 app.use("/api/messages",messageRoutes);
 
-if(process.env,NODE_ENV==="Production"){
+if(process.env.NODE_ENV==="Production"){
     app.use(express.static(path.join(__dirname,"..frontend/dist")));
 
     app.get("*",(req,res)=>{
